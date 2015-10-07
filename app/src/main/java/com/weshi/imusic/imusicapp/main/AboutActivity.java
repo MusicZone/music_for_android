744f4b36be50eab0a75752c43c372b935ff10f71
liweichen <tomtomasgreen@hotmail.com>
2015-10-07 11:58:39 +0800

    add about

---
+++ app/src/main/java/com/weshi/imusic/imusicapp/main/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.weshi.imusic.imusicapp.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.weshi.imusic.imusicapp.R;
+
+public class AboutActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
